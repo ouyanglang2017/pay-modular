82419be7b086263211b6b09bb4eb4d5a7c57d254
ouyanglang <1041212092@qq.com>
2019-02-14 11:41:44 +0800

    更新打包内容

--- src/main/java/com/startai/controller/TestController.java
+++ src/main/java/com/startai/controller/TestController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @RequestMapping("/test")
     public String test() {
-        return "test success";
+
+        return "test success！！！！";
     }
 }
